b4834e003cc5461f29be08b9f2e216b1efb00cf3
Vulkano <vulkano@tuta.io>
2023-09-02 21:43:27 +0200

    Updates made on matebook 14s running arch and dwm

--- neovim/.config/nvim/lua/keymaps.lua
+++ neovim/.config/nvim/lua/keymaps.lua
@@ -1,5 +1,6 @@
 vim.keymap.set('n', '<c-n>', ':tabnew <CR>')
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
+vim.keymap.set('n', '<CR>', 'o<Esc>')
 --vim.keymap.set('i', 'jj', '<ESC>')
 --vim.keymap.set('i', '<esc>', '<nop>')
