a2ea91b44e39576a897207a31ae5c0bf02487bac
Vulkano <vulkano@tuta.io>
2024-02-22 21:49:05 +0100

    List treesitter parsers vertically(cosmetic change)

--- neovim/.config/nvim/lua/treesitter.lua
+++ neovim/.config/nvim/lua/treesitter.lua
@@ -1,6 +1,21 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "make", "cmake", "cpp", "python", "toml", "yaml", "json", "javascript", "go", "typescript", "markdown", "markdown_inline"},
+  ensure_installed = { "c",
+                       "lua",
+                       "rust",
+                       "make",
+                       "cmake",
+                       "cpp",
+                       "python",
+                       "toml",
+                       "yaml",
+                       "json",
+                       "javascript",
+                       "go",
+                       "typescript",
+                       "markdown",
+                       "markdown_inline"
+                      },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
