34f17c5279a7ec8bf0591a97b5886bb432e13672
Matija Radovic <matija.radovic@skyworksinc.com>
2024-03-18 17:14:17 +0100

    Organize powershell files

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -4,16 +4,4 @@ Import-Module -Name Terminal-Icons
 Invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
-function jumpIntoNvimConfigFolder {
-    cd C:\Users\$Env:USERNAME\AppData\Local\nvim
-}
-
-function jumpIntoPsConfigFolder {
-    cd C:\Users\$Env:USERNAME\Documents\PowerShell
-}
-Set-Alias -Name vim     -Value nvim
-Set-Alias -Name v       -Value nvim
-Set-Alias -Name nvimcfg -Value jumpIntoNvimConfigFolder
-Set-Alias -Name pscfg   -Value jumpIntoPsConfigFolder
-
-$DotFilesPath = "C:\Users\radovim\dotfiles"
+Import-Module "C:\Users\$Env:USERNAME\Documents\PowerShell\aliases.ps1"
--- neovim/.config/nvim/lua/noice-conf.lua
+++ neovim/.config/nvim/lua/noice-conf.lua
@@ -1,4 +1,34 @@
 require("noice").setup({
+  views = {
+      cmdline_popup = {
+          position = {
+              row = 5,
+              col = "50%",
+          },
+          size = {
+              width = 60,
+              height = "auto",
+          },
+      },
+      popupmenu = {
+          relative = "editor",
+          position = {
+              row = 10,
+              col = "50%",
+          },
+          size = {
+              width = 60,
+              height = "auto",
+          },
+          border = {
+              style = "rounded",
+              padding = { 0, 1 },
+          },
+          win_options = {
+              winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+          },
+      },
+  },
   lsp = {
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
     override = {
