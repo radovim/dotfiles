4f6d0ef598b6da1591e434396d6e84533df26aa9
radovim <radovic@tuta.com>
2025-10-11 22:43:59 +0200

    Improve laptop screen toggling script

--- hyprland/.config/hypr/scripts/toggle_laptop_screen.sh
+++ hyprland/.config/hypr/scripts/toggle_laptop_screen.sh
@@ -1,16 +1,43 @@
 #!/bin/sh
 
-internal_screen_name="eDP-1"
+laptop_monitor="eDP-1"
 resolution="prefferd"
-position="698x1728"
+position_main="0x0"
+position_down="698x1728"
 scale="1.25"
 
-screen_state=$(hyprctl monitors | grep $internal_screen_name)
+monitors=$(hyprctl monitors | /usr/bin/grep -oP "(?<=Monitor\s)([^\s]+)(?=\s\()")
+readarray -t monitors <<< "$monitors"
 
-if [ -n "$screen_state" ]
-then
-    hyprctl keyword monitor eDP-1,disable
-else
-    hyprctl keyword monitor eDP-1,$resolution,$position,$scale
+echo "All monitors: ${monitors[@]}"
+echo "Number of monitors: ${#monitors[@]}"
+
+laptop_monitor_state="disabled"
+for monitor in ${monitors[@]}; do
+    if [ "$monitor" = "$laptop_monitor" ]; then
+        echo "Found: $monitor=$laptop_monitor"
+        laptop_monitor_state="enabled"
+        break
+    fi
+done
+
+if [ "$laptop_monitor_state" = "enabled" ]; then
+    echo "eDP-1 is ACTIVE"
+    # If @laptop_monitor is only active monitor, don't disable it
+    if [ ${#monitors[@]} -gt 1 ]; then
+        echo "DISABLING..."
+        hyprctl keyword monitor $laptop_monitor,disable
+    fi
+elif [ "$laptop_monitor_state" = "disabled" ]; then
+    echo "eDP-1 is NOT ACTIVE"
+    if [ ${#monitors[@]} -gt 0 ]; then
+        echo "Setting DOWN position"
+        position=$position_down
+    else
+        echo "Setting MAIN position"
+        position=$position_main
+    fi
+    echo "ENABLING..."
+    hyprctl keyword monitor $laptop_monitor,$resolution,$position,$scale
 fi
 
