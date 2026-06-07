396a4a05f63be7f06020c6a8b78d4c119c92ba20
radovim <radovic@tuta.com>
2026-06-07 16:01:56 +0200

    Add script that print number of packages for which update is available

--- sway/.config/sway/status.sh
+++ sway/.config/sway/status.sh
@@ -6,9 +6,9 @@ time=$(date +'%H:%M:%S')
 battery_status=$(~/.config/sway/scripts/battery.sh)
 network_status=$(~/.config/sway/scripts/network_status.sh)
 volume=$(~/.config/sway/scripts/volume.sh)
-
+packages=$(~/.config/sway/scripts/packages.sh)
 _sep_="|"
-echo "$_spe_ $volume $_sep_ $network_status $_sep_ $kb_layout $_sep_ $battery_status $_sep_   $date    $time $_sep_"
+echo "$volume $_sep_ $network_status $_sep_ $packages $_sep_ $kb_layout $_sep_ $battery_status $_sep_   $date    $time $_sep_"
 
 
 
